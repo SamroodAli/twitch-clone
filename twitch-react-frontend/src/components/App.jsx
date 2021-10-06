e2d7ee607f0618c5706bd031c79d8716b6bb5b63
samroodAli <devsamrood@gmail.com>
2021-10-06 18:59:11 +0530

    Header:Create and render placeholder header

--- twitch-react-frontend/src/components/App.jsx
+++ twitch-react-frontend/src/components/App.jsx
@@ -5,9 +5,19 @@ import StreamShow from "./streams/StreamShow";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
+import Header from "./Header";
 
 const App = () => {
-  return <Router></Router>;
+  return (
+    <Router>
+      <Header />
+      <Route exact path="/" component={StreamList} />
+      <Route path="/streams/show" component={StreamShow} />
+      <Route path="/streams/new" component={StreamCreate} />
+      <Route path="/streams/edit" component={StreamEdit} />
+      <Route path="/streams/delete" component={StreamDelete} />
+    </Router>
+  );
 };
 
 export default App;
