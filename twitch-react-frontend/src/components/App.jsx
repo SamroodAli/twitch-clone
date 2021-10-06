a758a1002475e61cd59816438f6ae8cb931c075e
samroodAli <devsamrood@gmail.com>
2021-10-06 18:50:38 +0530

    Import:Stream components into app.jsx

--- twitch-react-frontend/src/components/App.jsx
+++ twitch-react-frontend/src/components/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import StreamList from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
 
 const PageOne = () => {
   return <div>Page One</div>;
