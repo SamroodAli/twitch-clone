62e9c6dd17c30d5e621bbd437f14c054efee18bb
samroodAli <devsamrood@gmail.com>
2021-10-07 20:18:59 +0530

    Reducer:Auth reducer to handle userId as well

--- twitch-react-frontend/src/reducers/authReducer.js
+++ twitch-react-frontend/src/reducers/authReducer.js
@@ -2,12 +2,13 @@ import { SIGN_IN, SIGN_OUT } from "../actions/types";
 
 const INITIAL_STATE = {
   isSignedIn: null,
+  userId: null,
 };
 
 const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
-      return { ...state, isSignedIn: true };
+      return { ...state, isSignedIn: true, userId: action.payload.userId };
     case SIGN_OUT:
       return { ...state, isSignedIn: false };
     default:
