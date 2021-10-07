8fd6e9dee5ca4c260a5998f26c9b30374cd2e639
samroodAli <devsamrood@gmail.com>
2021-10-07 19:03:12 +0530

    Authentication:Wire sign in and sign out buttons with auth instance

--- twitch-react-frontend/src/components/GoogleAuth.jsx
+++ twitch-react-frontend/src/components/GoogleAuth.jsx
@@ -24,20 +24,28 @@ class GoogleAuth extends Component {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button className="ui red google button" onClick={this.onSignOut}>
           <i className="google icon" />
           Sign Out
         </button>
       );
     } else {
       return (
-        <button className="ui red google button">
+        <button className="ui red google button" onClick={this.onSignIn}>
           <i className="google icon" />
-          Sign in
+          Sign in with Google
         </button>
       );
     }
   }
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
 
   render() {
     return <div>{this.renderAuthButton()}</div>;
