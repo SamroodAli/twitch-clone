dab555e3955d1398b52862975e4a58744219b1a3
samroodAli <devsamrood@gmail.com>
2021-10-07 16:10:11 +0530

    Authentication:Get auth instance from gapi and get state isSignedin

--- twitch-react-frontend/src/components/GoogleAuth.jsx
+++ twitch-react-frontend/src/components/GoogleAuth.jsx
@@ -1,11 +1,32 @@
 import { Component } from "react";
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
-    window.gapi.load("client:auth2", () => {});
+    window.gapi.load("client:auth2", async () => {
+      await window.gapi.client.init({
+        clientId:
+          "223090150646-46o3fcmm064vrj3se89njlrm9tfd068c.apps.googleusercontent.com",
+        scope: "email",
+      });
+      this.auth = window.gapi.auth2.getAuthInstance();
+      this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div> I dont know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div> I am signed in</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
