c4e2d9c6238a0f91d00d199fe5049f7b36304982
samroodAli <devsamrood@gmail.com>
2021-10-08 14:13:26 +0530

    Test: Add redux devtools inside project

--- twitch-react-frontend/src/index.js
+++ twitch-react-frontend/src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "./reducers";
-const store = createStore(rootReducer);
+import { createStore, applyMiddleware, compose } from "redux";
+
+import reducer from "./reducers";
 const root = document.getElementById("root");
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware()));
 ReactDOM.render(
   <Provider store={store}>
     <App />
