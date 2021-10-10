89ffc7ab15b312e5d706a5980f49ea124509d900
samroodAli <devsamrood@gmail.com>
2021-10-10 21:56:51 +0530

    ErrorInForm:Get validation errors from validate function and present in meta

--- twitch-react-frontend/src/components/streams/StreamCreate.jsx
+++ twitch-react-frontend/src/components/streams/StreamCreate.jsx
@@ -2,17 +2,19 @@ import { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
+    console.log(meta);
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     );
   }
 
   onSubmit(formValues) {
-    console.log(formValues);
+    // console.log(formValues);
   }
 
   render() {
@@ -38,7 +40,6 @@ const validate = (formValues) => {
   if (!formValues.title) {
     errors.title = "You must enter a title";
   }
-
   if (!formValues.description) {
     errors.description = "You must enter a description";
   }
