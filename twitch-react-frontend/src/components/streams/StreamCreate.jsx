ca0221d76af39d599651d76b04e7747c8bd76340
samroodAli <devsamrood@gmail.com>
2021-10-10 21:35:48 +0530

    Validation:Create validate function that returns empty object or error messages

--- twitch-react-frontend/src/components/streams/StreamCreate.jsx
+++ twitch-react-frontend/src/components/streams/StreamCreate.jsx
@@ -33,6 +33,18 @@ class StreamCreate extends Component {
   }
 }
 
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+  return errors;
+};
+
 export default reduxForm({
   form: "streamCreate",
 })(StreamCreate);
