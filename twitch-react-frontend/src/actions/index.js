425f9880b99518eaf69d6272717442179b52eb8e
samroodAli <devsamrood@gmail.com>
2021-10-11 16:38:03 +0530

    ActionCreators:Delete a stream action creator

--- twitch-react-frontend/src/actions/index.js
+++ twitch-react-frontend/src/actions/index.js
@@ -44,3 +44,8 @@ export const editStream = (id, formValues) => async (dispatch) => {
   const response = await streams.put(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: { data: response.data } });
 };
+
+export const deleteStream = (id) => async (dispatch) => {
+  const response = await streams.delete(`/streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: response.data });
+};
