eddb4aca6e34aa3eaa2e96dab87ff3bf1900a17a
samroodAli <devsamrood@gmail.com>
2021-10-11 19:28:42 +0530

    Fix: Move reducer file from actions to reducer

--- twitch-react-frontend/src/reducers/streamReducer.js
+++ twitch-react-frontend/src/reducers/streamReducer.js
@@ -1,15 +1,17 @@
+import _ from "lodash";
+
 import {
   CREATE_STREAM,
   DELETE_STREAM,
   EDIT_STREAM,
   FETCH_STREAM,
   FETCH_STREAMS,
-} from "./types";
+} from "../actions/types";
 
 const streamsReducer = (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:
-      return action.payload;
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
@@ -17,7 +19,7 @@ const streamsReducer = (state = {}, action) => {
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case DELETE_STREAM:
-      return { ...state, [action.payload.id]: undefined };
+      return _.omit(state, action.payload.id);
     default:
       return state;
   }
