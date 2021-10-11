7ca15fd41c1c7c2e8bed7442cda0c4c9a40744c1
samroodAli <devsamrood@gmail.com>
2021-10-11 19:11:32 +0530

    Fix: Fix unused response variable in delete function

--- twitch-react-frontend/src/actions/index.js
+++ twitch-react-frontend/src/actions/index.js
@@ -46,6 +46,6 @@ export const editStream = (id, formValues) => async (dispatch) => {
 };
 
 export const deleteStream = (id) => async (dispatch) => {
-  const response = await streams.delete(`/streams/${id}`);
+  await streams.delete(`/streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: { id } });
 };
