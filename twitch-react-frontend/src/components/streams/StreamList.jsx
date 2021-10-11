7cf1d10a1e168282a5a725bb8ec07ce462bf4466
samroodAli <devsamrood@gmail.com>
2021-10-11 19:56:47 +0530

    Fix: Render StreamList and fix reducer state key stream to streams

--- twitch-react-frontend/src/components/streams/StreamList.jsx
+++ twitch-react-frontend/src/components/streams/StreamList.jsx
@@ -8,13 +8,31 @@ class StreamList extends Component {
     this.props.fetchStreams();
   }
 
+  renderStreams = () =>
+    this.props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+
   render() {
     return (
       <div>
-        <p>Hello lists</p>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderStreams()}</div>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = ({ streams }) => {
+  console.log(streams);
+  return { streams: Object.values(streams) };
+};
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
