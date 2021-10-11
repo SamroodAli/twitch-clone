fbd59187595b84a7029b825ab3c8ccd626fefeee
samroodAli <devsamrood@gmail.com>
2021-10-11 20:44:30 +0530

    Edit/delete:Render edit and delete buttons for current User

--- twitch-react-frontend/src/components/streams/StreamList.jsx
+++ twitch-react-frontend/src/components/streams/StreamList.jsx
@@ -9,7 +9,12 @@ class StreamList extends Component {
 
   renderAdmin = (stream) => {
     if (stream.userId === this.props.currentUserId) {
-      return "DELETE/EDIT";
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
     }
   };
 
@@ -17,12 +22,12 @@ class StreamList extends Component {
     this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
-          {this.renderAdmin(stream)}
         </div>
       );
     });
