c5dd9b4bc4872edaaf1bc29ac509eb887956f3d9
samroodAli <devsamrood@gmail.com>
2021-10-11 22:50:16 +0530

    Edit:Give edit page an id parameter

--- twitch-react-frontend/src/components/App.jsx
+++ twitch-react-frontend/src/components/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <Route exact path="/" component={StreamList} />
       <Route path="/streams/show" component={StreamShow} />
       <Route path="/streams/new" component={StreamCreate} />
-      <Route path="/streams/edit" component={StreamEdit} />
+      <Route path="/streams/edit/:id" component={StreamEdit} />
       <Route path="/streams/delete" component={StreamDelete} />
     </Router>
   );
