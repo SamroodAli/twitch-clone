e23722630650b78bccd75fd9312094e59bca6f8b
samroodAli <devsamrood@gmail.com>
2021-10-11 22:14:55 +0530

    History:Create history object and pass it to Router instead of BrowserRouter

--- twitch-react-frontend/src/components/App.jsx
+++ twitch-react-frontend/src/components/App.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import Header from "./Header";
+import history from "../history";
 
 const App = () => {
   return (
-    <Router>
+    <Router history={history}>
       <Header />
       <Route exact path="/" component={StreamList} />
       <Route path="/streams/show" component={StreamShow} />
--- twitch-react-frontend/src/history.js
+++ twitch-react-frontend/src/history.js
@@ -1,2 +1,2 @@
-import { createHistory } from "history";
-export default createHistory();
+import { createBrowserHistory } from "history";
+export default createBrowserHistory();
