799a48d342e7e1682eb346236d4ad70560cd37e4
samroodAli <devsamrood@gmail.com>
2021-10-11 16:34:20 +0530

    ActionCreators:Fetch stream action creator

--- twitch-react-frontend/src/actions/index.js
+++ twitch-react-frontend/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../api/streams";
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from "./types";
 
 export const signIn = (userId) => {
   return {
@@ -20,4 +28,9 @@ export const createStream = (formValues) => async (dispatch) => {
     formValues
   );
   dispatch({ action: CREATE_STREAM, payload: { data: response.data } });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: { data: response.data } });
 };
