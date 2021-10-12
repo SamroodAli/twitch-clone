b2045429d97ab3a9dcd409d47e867225dc0ef854
samroodAli <devsamrood@gmail.com>
2021-10-12 15:07:08 +0530

    Refactor: Refactor jsx fragment to use verbose form of fragment

--- twitch-react-frontend/src/components/streams/StreamDelete.jsx
+++ twitch-react-frontend/src/components/streams/StreamDelete.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Modal from "../Modal";
 const StreamDelete = () => {
   const actions = (
-    <div>
+    <React.Fragment>
       <button className="ui button negative">Delete</button>
       <button className="ui button ">Cancel</button>
-    </div>
+    </React.Fragment>
   );
   return (
     <div>
