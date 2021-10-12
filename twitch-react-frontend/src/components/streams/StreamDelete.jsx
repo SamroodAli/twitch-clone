b9be8032e06d86851b572212f594c2c7e31aa7a3
samroodAli <devsamrood@gmail.com>
2021-10-12 15:19:04 +0530

    Delete:FetchStream actionCreator called on delete url page

--- twitch-react-frontend/src/components/streams/StreamDelete.jsx
+++ twitch-react-frontend/src/components/streams/StreamDelete.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Modal from "../Modal";
 import history from "../../history";
+import { fetchStream } from "../../actions";
+import { connect } from "react-redux";
 
 class StreamDelete extends React.Component {
-  actions = () => {
+  renderActions = () => {
     return (
       <React.Fragment>
         <button className="ui button negative">Delete</button>
@@ -12,13 +14,18 @@ class StreamDelete extends React.Component {
     );
   };
 
+  componentDidMount() {
+    const streamId = this.props.match.params.id;
+    this.props.fetchStream(streamId);
+  }
+
   render = () => (
     <div>
       Delete streams
       <Modal
         title="Delete stream"
         content="Are you sure you want to delete this stream ?"
-        actions={this.actions}
+        actions={this.renderActions()}
         onDismiss={() => {
           history.push("/");
         }}
@@ -27,4 +34,4 @@ class StreamDelete extends React.Component {
   );
 }
 
-export default StreamDelete;
+export default connect(null, { fetchStream })(StreamDelete);
