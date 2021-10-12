1ab58104a80890222beb4c3b67d74f468609c7a6
samroodAli <devsamrood@gmail.com>
2021-10-12 14:58:33 +0530

    StopEventPropogation:Outside programmatically navigates to home page

--- twitch-react-frontend/src/components/Modal.jsx
+++ twitch-react-frontend/src/components/Modal.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
+    <div
+      className="ui dimmer modals visible active"
+      onClick={() => {
+        history.push("/");
+      }}
+    >
+      <div
+        className="ui standard modal visible active"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
         <div className="header">Delete Stream</div>
         <div className="content">
           Are you sure you want to delete this stream ?
