38e7fb016dfb7316d8fe9959b8432b5f2c3091e5
samroodAli <devsamrood@gmail.com>
2021-10-12 15:13:10 +0530

    Delete:Refactor streamDelete to a class component

--- twitch-react-frontend/src/components/streams/StreamDelete.jsx
+++ twitch-react-frontend/src/components/streams/StreamDelete.jsx
@@ -2,26 +2,29 @@ import React from "react";
 import Modal from "../Modal";
 import history from "../../history";
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button ">Cancel</button>
-    </React.Fragment>
-  );
-  return (
+class StreamDelete extends React.Component {
+  actions = () => {
+    return (
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button ">Cancel</button>
+      </React.Fragment>
+    );
+  };
+
+  render = () => (
     <div>
       Delete streams
       <Modal
         title="Delete stream"
         content="Are you sure you want to delete this stream ?"
-        actions={actions}
+        actions={this.actions}
         onDismiss={() => {
           history.push("/");
         }}
       />
     </div>
   );
-};
+}
 
 export default StreamDelete;
