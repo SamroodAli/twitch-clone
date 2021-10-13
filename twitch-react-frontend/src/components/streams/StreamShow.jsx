e9540571acd451a60ffe7a987b4a429f84543dac
samroodAli <devsamrood@gmail.com>
2021-10-13 16:33:43 +0530

    Comment:Player load option as there are no streams

--- twitch-react-frontend/src/components/streams/StreamShow.jsx
+++ twitch-react-frontend/src/components/streams/StreamShow.jsx
@@ -20,18 +20,21 @@ class StreamShow extends React.Component {
     this.buildPlayer();
   }
 
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
   buildPlayer = () => {
     if (this.player || !this.props.stream) {
       return;
     }
     const { id } = this.props.match.params;
-
     this.player = flv.createPlayer({
       type: "flv",
       url: `http://localhost:8000/live/${id}.flv`,
     });
     this.player.attachMediaElement(this.videoRef.current);
-    this.player.load();
+    // this.player.load;
   };
 
   render() {
