04ffbd0b318455be9257b449da03fea8a4cdec74
samroodAli <devsamrood@gmail.com>
2021-10-13 15:45:40 +0530

    Fix: Fix null ref object for video when no streaming

--- twitch-react-frontend/src/components/streams/StreamShow.jsx
+++ twitch-react-frontend/src/components/streams/StreamShow.jsx
@@ -10,15 +10,29 @@ class StreamShow extends React.Component {
   }
 
   componentDidMount() {
+    console.log(this.videoRef);
     const { id } = this.props.match.params;
     this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  buildPlayer = () => {
+    if (this.player || !this.stream.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+
     this.player = flv.createPlayer({
       type: "flv",
       url: `http://localhost:8000/live/${id}.flv`,
     });
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
-  }
+  };
 
   render() {
     if (!this.props.stream) {
