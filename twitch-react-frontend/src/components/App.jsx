a080516d2505a33ace835d2e7f8e493a3a973da4
samroodAli <devsamrood@gmail.com>
2021-10-13 13:42:36 +0530

    Fix: Fix react rendering two matching components with Router using switch

--- twitch-react-frontend/src/components/App.jsx
+++ twitch-react-frontend/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import StreamCreate from "./streams/StreamCreate";
@@ -12,11 +12,13 @@ const App = () => {
   return (
     <Router history={history}>
       <Header />
-      <Route exact path="/" component={StreamList} />
-      <Route path="/streams/show" component={StreamShow} />
-      <Route path="/streams/new" component={StreamCreate} />
-      <Route path="/streams/edit/:id" component={StreamEdit} />
-      <Route path="/streams/delete/:id" component={StreamDelete} />
+      <Switch>
+        <Route exact path="/" component={StreamList} />
+        <Route path="/streams/new" component={StreamCreate} />
+        <Route path="/streams/edit/:id" component={StreamEdit} />
+        <Route path="/streams/delete/:id" component={StreamDelete} />
+        <Route path="/streams/:id" component={StreamShow} />
+      </Switch>
     </Router>
   );
 };
